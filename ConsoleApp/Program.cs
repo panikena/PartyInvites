6bbb12baab708cb08f10602e2e9cf589454201de
panikena <adenisenko@ymail.com>
2016-05-25 17:58:19 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using ServiceCoursework.Core;
+using ServiceCoursework.ServiceLibrary;
+using Newtonsoft.Json;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user;
+            IUserService service = new UserService();
+            Console.WriteLine(service.GetUser("1") + service.GetUser("2"));
+          //  user = (User) JsonConvert.DeserializeObject(service.GetUser("1"));
+
+           // Console.WriteLine(user.Id);
+           // Console.WriteLine(user.Password);
+           // Console.WriteLine(user.Email);
+            //Console.WriteLine(user.Phone);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServiceCoursework.Site/Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ServiceCoursework.Site.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите свое имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите телефон")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите, примите ли участие в вечеринке")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
